3654e9d2365880d1fa180a738b00d2928998028e
风魔小次郎 <XHFkindergarten@qq.com>
2020-05-06 21:05:23 +0800

    feat: developing

--- src/pages/main/testModule.js
+++ src/pages/main/testModule.js
@@ -1,37 +1,14 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native'
-import ColorPicker from 'src/components/colorPicker'
+import Test from './test.svg'
 
 export default function TestModule() {
-  const [ showSelect, setShowSelect ] = useState(false)
-  const handlePress = () => {
-    setShowSelect(true)
-  }
-  const handleOk = color => {
-    console.log(color)
-    setShowSelect(false)
-  }
-  const handleCancel = () => {
-    setShowSelect(false)
-  }
   return (
     <View style={ {
       height: 100
     } }
     >
-      <TouchableOpacity onPress={ handlePress }>
-        <Text style={ {
-          color: '#FFF',
-          fontSize: 20
-        } }
-        >颜色选择器</Text>
-      </TouchableOpacity>
-      <ColorPicker
-        color="#FF0000"
-        onCancel={ handleCancel }
-        onOk={ handleOk }
-        visible={ showSelect }
-      />
+      <Test />
     </View>
   )
 }
